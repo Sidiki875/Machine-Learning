59a4e16989c0374832fa1d28615ca77d533eaf45
Sidikiba Diane <sidikibadianey2@gmail.com>
2026-06-23 03:07:56 +0000

    defined

--- mainproject/healthapp/admin.py
+++ mainproject/healthapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import User
-
+from .models import Participant3
 # Register your models here.
 
 admin.site.register(User)
+admin.site.register(Participant3)
